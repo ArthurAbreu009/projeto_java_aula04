6ddbca387eb1b0d6c8ec4dd69061a74a0f6ec9d6
Arthur <arthur.abreu@duque.g12.br>
2025-08-25 19:40:33 -0300

    Primeiro commit da aplicação

---
+++ aula04/src/main/java/com/arthur/aula04/Aula04.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.arthur.aula04;
+
+/**
+ *
+ * @author 08287
+ */
+public class Aula04 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
